f20fa1da5df17b308e3c5675edfd5891c28071b9
unknown <thanhhuy12211331@gmail.com>
2021-08-25 00:31:24 +0700

    chuoi lap tai len lan 2

--- chuoilap.py
+++ chuoilap.py
@@ -1,17 +1,25 @@
 A = input("Nhập chuỗi: ")
-d=len(A)//2
-if len(A)%2==0:
-    for b in range (1,d+1):
-        e=0#số lần lặp
-        for i in range (0,d):
-            if A[:b]==A[b:b*2]:
-                e=e+1
+n=len(A)
+d=n//2
+for b in range (1,d+1):
+    e=0
+    f=n//b#số lần lặp để xét chuỗi con
+    g=0
+    for i in range (0,f):
+        if g==0:
+            z=b
+        if A[:b]==A[z:z+b]:
+            e=e+1
+            g+=1
+        else:
+            if A[z:z+b]!="":
+                e=0
+                break
             else:
                 break
-        if e==d:
-            print("Chuỗi lặp đi lặp lại bởi chuỗi",A[:b],"và lặp lại",e,"lần")
-            break
-    if e==0:
-        print("Không phải chuỗi lặp")
-else:
+        z+=b
+    if e==f-1:
+        print("Chuỗi lặp đi lặp lại bởi chuỗi",A[:b],"và lặp lại",n/b,"lần")
+        break
+if e==0:
     print("Không phải chuỗi lặp")
